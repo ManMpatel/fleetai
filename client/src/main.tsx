2b5cfdc186e2cbf9a659ee40ea08466faf2d3de0
ManMpatel <manmpatel1@gmail.com>
2026-04-25 17:40:42 +1000

    Add PWA support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -21,3 +21,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </Auth0Provider>
   </React.StrictMode>
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
